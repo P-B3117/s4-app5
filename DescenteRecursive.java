f25455748520cf4c5776a705145a2ce56f29bc90
PB-3117 <zacse38@gmail.com>
2025-07-14 14:07:45 -0400

    fixed teachers project

--- DescenteRecursive.java
+++ DescenteRecursive.java
@@ -1,4 +1,4 @@
-package app;6
+package app6;
 
 /** @author Ahmed Khoumsi */
 
@@ -6,67 +6,60 @@ package app;6
  */
 public class DescenteRecursive {
 
-  // Attributs
+    // Attributs
 
-/** Constructeur de DescenteRecursive :
+    /** Constructeur de DescenteRecursive :
       - recoit en argument le nom du fichier contenant l'expression a analyser
       - pour l'initalisation d'attribut(s)
  */
-public DescenteRecursive(String in) {
-    //
-}
-
-
-/** AnalSynt() effectue l'analyse syntaxique et construit l'AST.
- *    Elle retourne une reference sur la racine de l'AST construit
- */
-public ElemAST AnalSynt( ) {
-   //
-}
-
-
-// Methode pour chaque symbole non-terminal de la grammaire retenue
-// ... 
-// ...
-
-
-
-/** ErreurSynt() envoie un message d'erreur syntaxique
- */
-public void ErreurSynt(String s)
-{
-    //
-}
-
-
-
-  //Methode principale a lancer pour tester l'analyseur syntaxique 
-  public static void main(String[] args) {
-    String toWriteLect = "";
-    String toWriteEval = "";
-
-    System.out.println("Debut d'analyse syntaxique");
-    if (args.length == 0){
-      args = new String [2];
-      args[0] = "ExpArith.txt";
-      args[1] = "ResultatSyntaxique.txt";
+    public DescenteRecursive(String in) {
+        //
     }
-    DescenteRecursive dr = new DescenteRecursive(args[0]);
-    try {
-      ElemAST RacineAST = dr.AnalSynt();
-      toWriteLect += "Lecture de l'AST trouve : " + RacineAST.LectAST() + "\n";
-      System.out.println(toWriteLect);
-      toWriteEval += "Evaluation de l'AST trouve : " + RacineAST.EvalAST() + "\n";
-      System.out.println(toWriteEval);
-      Writer w = new Writer(args[1],toWriteLect+toWriteEval); // Ecriture de toWrite 
-                                                              // dans fichier args[1]
-    } catch (Exception e) {
-      System.out.println(e);
-      e.printStackTrace();
-      System.exit(51);
+
+    /** AnalSynt() effectue l'analyse syntaxique et construit l'AST.
+     *    Elle retourne une reference sur la racine de l'AST construit
+     */
+    public ElemAST AnalSynt() {
+        //
     }
-    System.out.println("Analyse syntaxique terminee");
-  }
 
+    // Methode pour chaque symbole non-terminal de la grammaire retenue
+    // ...
+    // ...
+
+    /** ErreurSynt() envoie un message d'erreur syntaxique
+     */
+    public void ErreurSynt(String s) {
+        //
+    }
+
+    //Methode principale a lancer pour tester l'analyseur syntaxique
+    public static void main(String[] args) {
+        String toWriteLect = "";
+        String toWriteEval = "";
+
+        System.out.println("Debut d'analyse syntaxique");
+        if (args.length == 0) {
+            args = new String[2];
+            args[0] = "ExpArith.txt";
+            args[1] = "ResultatSyntaxique.txt";
+        }
+        DescenteRecursive dr = new DescenteRecursive(args[0]);
+        try {
+            ElemAST RacineAST = dr.AnalSynt();
+            toWriteLect +=
+                "Lecture de l'AST trouve : " + RacineAST.LectAST() + "\n";
+            System.out.println(toWriteLect);
+            toWriteEval +=
+                "Evaluation de l'AST trouve : " + RacineAST.EvalAST() + "\n";
+            System.out.println(toWriteEval);
+            Writer w = new Writer(args[1], toWriteLect + toWriteEval); // Ecriture de toWrite
+            // dans fichier args[1]
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+            System.exit(51);
+        }
+        System.out.println("Analyse syntaxique terminee");
+    }
 }
-
