2bd0c7f61ecb40af42eb6e9c21e9ad406ac1bc2c
PB-3117 <zacse38@gmail.com>
2025-07-16 16:26:05 -0400

    now supports nix run

--- src/AnalLex.java
+++ src/AnalLex.java
@@ -7,13 +7,27 @@ package src;
 public class AnalLex {
 
     // Attributs
-    //  ...
+    private String chaine;
+    private String output;
 
     /** Constructeur pour l'initialisation d'attribut(s)
      */
-    public AnalLex(String r) {
-        // arguments possibles
-        //
+    public AnalLex(String in) {
+        String toWrite = "";
+        System.out.println("Debut d'analyse lexicale");
+
+        Reader r = new Reader(in);
+        chaine = r.toString();
+
+        // Execution de l'analyseur lexical
+        Terminal t = null;
+        while (this.resteTerminal()) {
+            t = this.prochainTerminal();
+            toWrite += t.toString() + "\n"; // toWrite contient le resultat
+        } //    d'analyse lexicale
+        System.out.println(toWrite); // Ecriture de toWrite sur la console
+        output = toWrite;
+        System.out.println("Fin d'analyse lexicale");
     }
 
     /** resteTerminal() retourne :
@@ -21,7 +35,10 @@ public class AnalLex {
       true s'il reste encore au moins un terminal qui n'a pas ete retourne
  */
     public boolean resteTerminal() {
-        return false;
+        if (chaine == null || chaine.isEmpty() || chaine.length() == 1) {
+            return false;
+        }
+        return true;
     }
 
     /** prochainTerminal() retourne le prochain terminal
@@ -61,7 +78,9 @@ public class AnalLex {
     }
 
     private static boolean isOperator(char c) {
-        return (c == '+' || c == '-' || c == '*' || c == '/');
+        return (
+            c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')'
+        );
     }
 
     /** ErreurLex() envoie un message d'erreur lexicale
@@ -70,25 +89,14 @@ public class AnalLex {
 
     //Methode principale a lancer pour tester l'analyseur lexical
     public static void main(String[] args) {
-        String toWrite = "";
-        System.out.println("Debut d'analyse lexicale");
         if (args.length == 0) {
             args = new String[2];
             args[0] = "ExpArith.txt";
             args[1] = "ResultatLexical.txt";
         }
-        Reader r = new Reader(args[0]);
 
-        AnalLex lexical = new AnalLex(r.toString()); // Creation de l'analyseur lexical
+        AnalLex lexical = new AnalLex(args[0]); // Creation de l'analyseur lexical
 
-        // Execution de l'analyseur lexical
-        Terminal t = null;
-        while (lexical.resteTerminal()) {
-            t = lexical.prochainTerminal();
-            toWrite += t.chaine + "\n"; // toWrite contient le resultat
-        } //    d'analyse lexicale
-        System.out.println(toWrite); // Ecriture de toWrite sur la console
-        Writer w = new Writer(args[1], toWrite); // Ecriture de toWrite dans fichier args[1]
-        System.out.println("Fin d'analyse lexicale");
+        Writer w = new Writer(args[1], lexical.output); // Ecriture de toWrite dans fichier args[1]
     }
 }
