f4f1c77bfd4c2e916f94196be4b5566e486596be
Laurent Stéphenne <laurent@guibi.dev>
2025-07-16 14:36:23 -0400

    MacOS arm support

--- flake.nix
+++ flake.nix
@@ -8,7 +8,32 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in pkgs.stdenv.mkDerivation {
       name = "anallex";
-      src = ./.;
+      src = self;
+
+      buildInputs = [ pkgs.jdk21 ];
+
+      buildPhase = ''
+        mkdir -p bin
+        javac -d bin $(find . -name "*.java")
+        jar cvfm AnalLex.jar manifest.txt -C bin app6
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp AnalLex.jar $out/
+        cat > $out/bin/anallex <<EOF
+        #!${pkgs.runtimeShell}
+        exec ${pkgs.jdk21}/bin/java -jar $out/AnalLex.jar "\$@"
+        EOF
+        chmod +x $out/bin/anallex
+      '';
+    };
+
+    packages.aarch64-darwin.default = let
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+    in pkgs.stdenv.mkDerivation {
+      name = "anallex";
+      src = self;
 
       buildInputs = [ pkgs.jdk21 ];
 
@@ -32,6 +57,11 @@
     apps.x86_64-linux.default = {
       type = "app";
       program = "${self.packages.x86_64-linux.default}/bin/anallex";
+    };
+
+    apps.aarch64-darwin.default = {
+      type = "app";
+      program = "${self.packages.aarch64-darwin.default}/bin/anallex";
     };
   };
 }
